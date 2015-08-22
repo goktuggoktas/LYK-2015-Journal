01932e389204c50fb2cf52e1b37e242696173e4c
Göktuğ Göktaş <ertugrulgoktuggoktas@gmail.com>
2015-08-22 10:55:01 +0300

    First commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,47 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  def index
+    @posts = Post.all.order('created_at DESC')
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+
+    @post = current_user.posts.create(post_params)
+
+    if @post.save
+    redirect_to @post
+    else
+      render 'new'
+    end
+
+  end
+
+  def edit
+  end
+
+  def update
+      @post.update(post_params)
+        redirect_to root_path
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+  private
+    def post_params
+      params.require(:post).permit(:title, :body, :avatar)
+    end
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
+  resources :posts
+  root 'posts#index'
+end
