140d2c59982fb5409d4aef820aa4ecd071823b19
Göktuğ Göktaş <ertugrulgoktuggoktas@gmail.com>
2015-08-24 19:33:06 +0300

    bugfix rails admin

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,5 @@ class Post < ActiveRecord::Base
   has_attached_file :post_image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :post_image, :content_type => /\Aimage\/.*\Z/
   belongs_to :user
+  has_many :user_posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150823173459) do
+ActiveRecord::Schema.define(version: 20150824160624) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20150823173459) do
     t.string   "post_image_content_type"
     t.integer  "post_image_file_size"
     t.datetime "post_image_updated_at"
+  end
+
+  create_table "user_posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
