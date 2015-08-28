b8dca4763b8696716a08bd5754b1de2ebad9bf92
Göktuğ Göktaş <ertugrulgoktuggoktas@gmail.com>
2015-08-28 14:11:45 +0300

    Design of the index and show page

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   include Paperclip::Glue
   validates :title , presence: true , length: {minimum: 5}
   validates :body ,presence: true
-  has_attached_file :post_image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :post_image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "missing_post.png"
   validates_attachment_content_type :post_image, :content_type => /\Aimage\/.*\Z/
   belongs_to :user
 end
